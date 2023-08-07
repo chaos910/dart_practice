d485364d6e1bcd7df4e0dcf8cd7a6ec45222ae36
Dinura Sathsindu <dinurawork@gmail.com>
2023-08-07 14:54:33 +0530

    did some dart exercises

--- advanced.dart
+++ advanced.dart
@@ -7,12 +7,31 @@ class Cat {
   Cat(this.name);
 }
 
-void test() {
+void test() async {
   final meow = Cat('Fluffers');
   print(meow.name);
   meow.run();
   final foo = Person('Foo', 'Bar');
   print(foo.fullName);
+
+  final result = await heavyFutureMultiplyByTwo(
+      10); //await keyword is what fetch the data from a asynchronous functions
+  print(result);
+
+  //For streams 'await for' fetches the data inisde a stream
+  await for (final value in getName()) {
+    print(value);
+  }
+  ;
+  print("Stream finished successfully");
+
+  // Generators method call
+  for (final value in getNumbers()) {
+    print(value);
+    if (value == 2) {
+      break;
+    }
+  }
 }
 
 class Person {
@@ -21,22 +40,44 @@ class Person {
   Person(this.firstName, this.lastName);
 }
 
-extension FullName on Person {
-  String get fullName => "$firstName $lastName";
-}
-
 // Extentions
 // Extentions allow you to extend the functionalites in a previously written class by getting already defined functions and variables inside a class
 // You can implement the functionalities of the extention method inside the instances of the original classes
+
 extension Run on Cat {
   void run() {
     print('Cat $name is running');
   }
 }
 
+extension FullName on Person {
+  String get fullName => "$firstName $lastName";
+}
+
 // Future
-// Used in asyncoronus functions
+// Used in asynchronous functions
 
 Future<int> heavyFutureMultiplyByTwo(int a) {
   return Future.delayed(Duration(seconds: 3), () => a * 2);
+}
+
+// Streams
+
+Stream<String> getName() {
+  //return Stream.value('Foo');
+  return Stream.periodic(const Duration(seconds: 1), (value) {
+    return 'Foo';
+  });
+}
+
+// Generators
+
+List<int> getOneTwoThree() {
+  return [1, 2, 3];
+}
+
+Iterable<List<int>> getNumbers() sync* {
+  yield [1, 5];
+  yield [2];
+  yield [3];
 }
